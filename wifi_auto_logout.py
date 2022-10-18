84ec9012e0ec76e43fd444b3108e720583bcaf23
rndf <1061747240@qq.com>
2022-10-19 01:39:02 +0800

    打包完成之后可以执行

---
+++ wifi_auto_logout.py
@@ -0,0 +1,12 @@
+
+import time
+from main import WifiLogout
+
+username = input('请输入你的学号:')
+
+password = input('请输入你的密码:')
+
+user = WifiLogout(username=username, password=password)
+user.logout()
+
+# time.sleep(30)
